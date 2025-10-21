927e2dcf0e54b2945f038788fef75cd9efd215e1
Chidi <chidiebere@zap.africa>
2025-10-21 01:46:35 +0100

    fix:refactored the register-paths file

--- register-paths.js
+++ register-paths.js
@@ -1,8 +1,10 @@
 // register-paths.js
 const tsConfigPaths = require('tsconfig-paths');
 const tsConfig = require('./tsconfig.build.json');
+const path = require('path');
 
+// Adjust the baseUrl to point to the compiled "dist" folder
 tsConfigPaths.register({
-  baseUrl: tsConfig.compilerOptions.baseUrl,
+  baseUrl: path.resolve(__dirname, 'dist'),
   paths: tsConfig.compilerOptions.paths,
 });
